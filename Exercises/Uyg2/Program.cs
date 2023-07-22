50aea166b8aae0dd92ce4a4a7770664a4580b912
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-07-22 18:12:59 +0300

    Add files via upload

---
+++ Exercises/Uyg2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Klavyeden girilen iki sayinin ort 70 ve ustu ise iyi, degilse kotu yazan program
+
+            Console.WriteLine("1. not:");
+            int not1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("2. not:");
+            int not2 = Convert.ToInt32(Console.ReadLine());
+
+            int ort = (not1 + not2) / 2;
+            if (ort >= 70)
+            {
+                Console.WriteLine("Iyi!");
+            }
+            else
+            {
+                Console.WriteLine("Kotu!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
