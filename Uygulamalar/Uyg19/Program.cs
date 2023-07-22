04e889b6a6f5bb2deaf89fae284678d05b28142c
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-07-22 18:41:35 +0300

    Add files via upload

---
+++ Uygulamalar/Uyg19/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*20 elemanlı diziye rastgele -100 ile +100 arasında sayılar atan ve daha sonra dizinin elemanlarını,
+            pozitif sayı adedini, negatif sayı adedini yazdıran program*/
+
+            int[] numbers = new int[20];
+            int negative = 0, positive = 0;
+            Random rand = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                numbers[i] = rand.Next(-100, 100);
+            }
+
+            Console.Write("Dizi elemanları: ");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(numbers[i] + " ");
+                if(numbers[i] < 0)
+                {
+                    negative++;
+                }
+                else if (numbers[i] > 0)
+                {
+                    positive++;
+                }
+            }
+
+            Console.WriteLine("\nDizideki pozitif sayı adedi: " + positive);
+            Console.WriteLine("Dizideki negatif sayı adedi: " + negative);
+            Console.ReadLine();
+        }
+    }
+}
