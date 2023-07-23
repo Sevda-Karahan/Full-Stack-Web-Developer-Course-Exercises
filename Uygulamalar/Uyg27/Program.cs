848899f1fa6b93f2e8512cd562e3dafd7df0e5f2
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-07-23 17:08:59 +0300

    Add files via upload

---
+++ Uygulamalar/Uyg27/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soldier soldier1 = new Soldier();
+            Console.WriteLine("Ad soyad: ");
+            soldier1.ad_soyad = Console.ReadLine();
+            Console.WriteLine("Rutbe: ");
+            soldier1.rutbe = Console.ReadLine();
+            Console.WriteLine("Görev bölgesi: ");
+            soldier1.gorev_bolgesi = Console.ReadLine();
+            Console.WriteLine("Kan grubu: ");
+            soldier1.kan_grubu = Console.ReadLine();
+            Console.WriteLine("Hastalık durumu: ");
+            soldier1.hastalik_durumu = Console.ReadLine();
+            Console.WriteLine("Yas: ");
+            soldier1.yas = Console.ReadLine();
+            Console.WriteLine("Meslek: ");
+            soldier1.meslek = Console.ReadLine();
+
+            Soldier.uyari();
+            Soldier.rapor();
+
+            Console.Clear();
+            Console.WriteLine("Ad soyad: " + soldier1.ad_soyad);
+            Console.WriteLine("Rutbe: " + soldier1.rutbe);
+            Console.WriteLine("Görev bölgesi: " + soldier1.gorev_bolgesi);
+            Console.WriteLine("Kan grubu: " + soldier1.kan_grubu);
+            Console.WriteLine("Hastalık durumu: " + soldier1.hastalik_durumu);
+            Console.WriteLine("Yas: " + soldier1.yas);
+            Console.WriteLine("Meslek: " + soldier1.meslek);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Uygulamalar/Uyg27/Soldier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg27
+{
+    class Soldier
+    {
+        public string ad_soyad, rutbe, gorev_bolgesi, kan_grubu, hastalik_durumu, yas, meslek;
+
+        public static void uyari()
+        {
+            Console.WriteLine("Kan grubu 0Rh- ve 0Rh+ olanlar 1 saat fazla uyusun!");
+        }
+        
+        public static void rapor()
+        {
+            Console.WriteLine("Hastalığınız varsa cumartesi revir günüdür, bilginize!");
+        }
+    }
+}
