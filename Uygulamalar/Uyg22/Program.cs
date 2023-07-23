2b43027e7c31d42232d799050c150592e904e57d
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-07-23 08:24:15 +0300

    Add files via upload

---
+++ Uygulamalar/Uyg22/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg22
+{
+    class Program
+    {
+        public static void Alan(int kisak, int uzunk)
+        {
+            int alan = kisak * uzunk;
+            Console.WriteLine("Alan method sonucu: " + alan);
+        }
+
+        public static int Alan2(int x, int y)
+        {
+            return x*y;
+        }
+
+        static void Main(string[] args)
+        {
+            //METHODS
+
+            //dikdörtegin alanı
+
+            Alan(3, 5);
+            Console.WriteLine("Alan2 method sonucu: " + Alan2(3, 5));
+            Console.WriteLine("************");
+
+            Console.WriteLine("Kısa kenar değeri: ");
+            int kk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Uzun kenar değeri: ");
+            int uk = Convert.ToInt32(Console.ReadLine());
+           
+            Alan(kk, uk);
+            Console.WriteLine("Alan2 method sonucu: " + Alan2(kk, uk));
+            Console.ReadLine();
+
+        }
+    }
+}
