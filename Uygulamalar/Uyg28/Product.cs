19f2a85a3d2fb8f99b4838f8d34b0f40cda0e5a8
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-07-24 10:49:27 +0300

    Add files via upload

---
+++ Uygulamalar/Uyg28/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg28
+{
+    class Product
+    {
+        public int urun_no;
+        public string urun_adı, renk;
+        public float fiyat;
+        
+        public float kdvli_fiyat(float fiyat)
+        {
+            if (fiyat >= 100)
+            {
+                return fiyat * 1.05f;
+            }
+            else
+            {
+                return fiyat * 1.03f;
+            }
+        }
+    }
+}
---
+++ Uygulamalar/Uyg28/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+
+            Console.WriteLine("Urun no: ");
+            product1.urun_no = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Urun adı: ");
+            product1.urun_adı = Console.ReadLine();
+            Console.WriteLine("Urun rengi: ");
+            product1.renk = Console.ReadLine();
+            Console.WriteLine("Urun kdvsiz fiyatı: ");
+            product1.fiyat = Convert.ToSingle(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Urun no: " + product1.urun_no);
+            Console.WriteLine("Urun adı: " + product1.urun_adı);
+            Console.WriteLine("Urun rengi: " + product1.renk);
+            Console.WriteLine("Urun kdvsiz fiyatı: " + product1.fiyat);
+            Console.WriteLine("Urun kdvli fiyatı: " + product1.kdvli_fiyat(product1.fiyat));
+
+            Console.ReadLine();
+        }
+    }
+}
