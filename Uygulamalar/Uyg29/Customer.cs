45081b4fb47e7c93fc311206eab6cdcf0b1748c8
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-07-24 11:22:32 +0300

    Add files via upload

---
+++ Uygulamalar/Uyg29/Customer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg29
+{
+    class Customer
+    {
+        private int customer_no=0;
+        private string customer_name;
+        float balance=0; //varsayılan erişim şekli private olduğu için, private yazılmaması hataya sebep olmaz.
+
+        public int Customer_no
+        {
+            get
+            {
+                return customer_no;
+            }
+            set
+            {
+                if(value>=2 && value <= 100)
+                {
+                    customer_no = value;
+                }
+                else
+                {
+                    Console.WriteLine("2 ile 100 arasında değer giriniz.");
+                }
+            }
+        }
+
+        public string Customer_name
+        {
+            get;
+            set;
+        }
+
+        public float Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if(value>=1000 && value <= 2000)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Bakiyeniz 1000-2000 arasında olmalı, bakiyenizi güncellemelisiniz!");
+                }
+            }
+        }
+
+    }
+}
---
+++ Uygulamalar/Uyg29/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kapsülleme (Encapsulation)
+            Customer customer1 = new Customer();
+            Console.WriteLine("Müşteri no: ");
+            customer1.Customer_no = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Müşteri adı: ");
+            customer1.Customer_name = Console.ReadLine();
+            Console.WriteLine("Bakiye giriniz: ");
+            customer1.Balance = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Müşteri no: " + customer1.Customer_no);
+            Console.WriteLine("Müşteri adı: " + customer1.Customer_name);
+            Console.WriteLine("Müşteri bakiye: " + customer1.Balance);
+
+            Console.ReadLine();
+        }
+    }
+}
