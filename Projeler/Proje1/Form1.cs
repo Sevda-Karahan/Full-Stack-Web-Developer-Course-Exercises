31d8ad0bc6526941a12a41a510105a2ff2f28e7f
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-07-26 20:07:58 +0300

    Add files via upload

---
+++ Projeler/Proje1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad = textBox1.Text;
+            //string soyad = textBox2.Text;
+            //string meslek = textBox3.Text;
+            MessageBox.Show(ad + " Hosgeldin!");
+        }
+    }
+}
