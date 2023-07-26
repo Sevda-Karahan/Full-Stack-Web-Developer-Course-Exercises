f4fda019d445d51564d5263aca3ef81e185025bd
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-07-26 13:22:08 +0300

    Add files via upload

---
+++ Uygulamalar/Uyg33/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg33
+{
+    class Program
+    {
+        //POLYMORPHISM
+        // method does not take any parameter 
+        public void greet()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        // method takes one string parameter
+        public void greet(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+
+            // calls method without any argument
+            p1.greet();
+
+            //calls method with an argument
+            p1.greet("Tim");
+        }
+    }
+}
