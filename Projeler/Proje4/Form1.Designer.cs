1f97475dc8229cccdd5a2bc61f4b6774748b97e0
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-07-27 18:22:21 +0300

    Add files via upload

---
+++ Projeler/Proje4/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Proje4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Fatura = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kahveler:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Latte",
+            "Filtre",
+            "Türk Kahvesi"});
+            this.comboBox1.Location = new System.Drawing.Point(89, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(162, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(34, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Fiyat:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Satın Al";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Fatura
+            // 
+            this.Fatura.FormattingEnabled = true;
+            this.Fatura.Location = new System.Drawing.Point(124, 183);
+            this.Fatura.Name = "Fatura";
+            this.Fatura.Size = new System.Drawing.Size(127, 134);
+            this.Fatura.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(306, 334);
+            this.Controls.Add(this.Fatura);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Fatura;
+    }
+}
+
---
+++ Projeler/Proje4/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            groupBox1.Visible = true;
+            int fiyat = 0;
+            if (comboBox1.SelectedItem == "Latte")
+            {
+                //groupBox1.Visible = true;
+                fiyat = 120;
+            }
+            else if(comboBox1.SelectedItem == "Filtre")
+            {
+                fiyat = 100 ;
+            }
+            else
+            {
+                fiyat = 70;
+            }
+            textBox1.Text = fiyat.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            Fatura.Visible = false;
+            button1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fatura.Visible = true;
+            if(comboBox1.SelectedItem == "Latte")
+            {
+                Fatura.Items.Add("Kahve: " + comboBox1.SelectedItem);
+                Fatura.Items.Add("Fiyat: " + textBox1.Text);
+            }
+            else if (comboBox1.SelectedItem == "Filtre")
+            {
+                Fatura.Items.Add("Kahve: " + comboBox1.SelectedItem);
+                Fatura.Items.Add("Fiyat: " + textBox1.Text);
+            }
+            else
+            {
+                Fatura.Items.Add("Kahve: " + comboBox1.SelectedItem);
+                Fatura.Items.Add("Fiyat: " + textBox1.Text);
+            }
+        }
+    }
+}
