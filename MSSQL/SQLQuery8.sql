1c822df77c8b732d1d829934c0bea906bb52229a
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-08-01 22:40:07 +0300

    Update SQLQuery8.sql

--- MSSQL/SQLQuery8.sql
+++ MSSQL/SQLQuery8.sql
@@ -1,8 +1,49 @@
-create database Northwind
-use Northwind
-
---like: arama yapmak i�in kullan�l�r
+--like: arama yapmak için kullanılır
 --%A : a ile bitenleri getir
---A% : a ile ba�layanlar� getir
+--A% : a ile başlayanları getir
 
-create table Urunler
+create database DENEME
+use DENEME
+
+--Deneme_not_null isimli tablo
+--ID int
+--ad varchar
+--soyad varchar
+--telefon char(10)
+
+create table Deneme_not_null
+(
+	ID int primary key identity,
+	ad varchar(50),
+	soyad varchar(50),
+	telefon char(10) not null
+)
+
+--Deneme_Unique isimli tablo
+--ID int
+--TC char(11) UNIQUE
+--ad varchar
+--soyad varchar
+--telefon char(10)
+
+create table Deneme_Unique
+(
+	ID int primary key identity(100,1),
+	TC char(11) UNIQUE,
+	ad varchar(50),
+	soyad varchar(50),
+	telefon char(10) not null
+)
+
+--Deneme_Check isimli tablo
+--Id, ad, soyad, yas alanları
+--yas alanı 18 yasından büyük olanları kayıt edebilsin
+
+create table Deneme_Check
+(
+	Id int primary key identity(1,1),
+	ad varchar(50),
+	soyad varchar(50),
+	yas tinyint,
+	check (yas>=18)
+)
