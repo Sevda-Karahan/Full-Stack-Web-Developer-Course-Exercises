dce8a69ea9ad18f3b45ac317c0dd5c32cfbeba3c
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2023-08-08 16:46:35 +0300

    Add files via upload

---
+++ Ado.Net/Ado.Net(Prosedursuz)/Ado.Net(Prosedursuz)/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Ado.Net_Prosedursuz_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtAdi = new System.Windows.Forms.TextBox();
+            this.TxtTanim = new System.Windows.Forms.TextBox();
+            this.TxtSayi = new System.Windows.Forms.TextBox();
+            this.Tarih = new System.Windows.Forms.DateTimePicker();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnListele = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.BtnAra = new System.Windows.Forms.Button();
+            this.BtnTemizle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İştirak Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(32, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İştirak Tanım:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(32, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Çalışan Sayısı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(32, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Kuruluş Tarihi:";
+            // 
+            // TxtAdi
+            // 
+            this.TxtAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtAdi.Location = new System.Drawing.Point(171, 14);
+            this.TxtAdi.Name = "TxtAdi";
+            this.TxtAdi.Size = new System.Drawing.Size(140, 24);
+            this.TxtAdi.TabIndex = 4;
+            // 
+            // TxtTanim
+            // 
+            this.TxtTanim.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtTanim.Location = new System.Drawing.Point(171, 56);
+            this.TxtTanim.Name = "TxtTanim";
+            this.TxtTanim.Size = new System.Drawing.Size(140, 24);
+            this.TxtTanim.TabIndex = 4;
+            // 
+            // TxtSayi
+            // 
+            this.TxtSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSayi.Location = new System.Drawing.Point(171, 105);
+            this.TxtSayi.Name = "TxtSayi";
+            this.TxtSayi.Size = new System.Drawing.Size(140, 24);
+            this.TxtSayi.TabIndex = 4;
+            // 
+            // Tarih
+            // 
+            this.Tarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Tarih.Location = new System.Drawing.Point(171, 151);
+            this.Tarih.Name = "Tarih";
+            this.Tarih.Size = new System.Drawing.Size(140, 24);
+            this.Tarih.TabIndex = 5;
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(114, 219);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(80, 34);
+            this.BtnKaydet.TabIndex = 6;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Location = new System.Drawing.Point(155, 259);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(80, 34);
+            this.BtnSil.TabIndex = 7;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = true;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.Location = new System.Drawing.Point(27, 298);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(80, 34);
+            this.BtnListele.TabIndex = 8;
+            this.BtnListele.Text = "Listele";
+            this.BtnListele.UseVisualStyleBackColor = true;
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Location = new System.Drawing.Point(71, 259);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(78, 33);
+            this.BtnGuncelle.TabIndex = 9;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = true;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // BtnAra
+            // 
+            this.BtnAra.Location = new System.Drawing.Point(113, 299);
+            this.BtnAra.Name = "BtnAra";
+            this.BtnAra.Size = new System.Drawing.Size(80, 34);
+            this.BtnAra.TabIndex = 10;
+            this.BtnAra.Text = "Ara";
+            this.BtnAra.UseVisualStyleBackColor = true;
+            this.BtnAra.Click += new System.EventHandler(this.BtnAra_Click);
+            // 
+            // BtnTemizle
+            // 
+            this.BtnTemizle.Location = new System.Drawing.Point(199, 298);
+            this.BtnTemizle.Name = "BtnTemizle";
+            this.BtnTemizle.Size = new System.Drawing.Size(80, 34);
+            this.BtnTemizle.TabIndex = 11;
+            this.BtnTemizle.Text = "Temizle";
+            this.BtnTemizle.UseVisualStyleBackColor = true;
+            this.BtnTemizle.Click += new System.EventHandler(this.BtnTemizle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(344, 14);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(533, 346);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 375);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BtnTemizle);
+            this.Controls.Add(this.BtnAra);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.Tarih);
+            this.Controls.Add(this.TxtSayi);
+            this.Controls.Add(this.TxtTanim);
+            this.Controls.Add(this.TxtAdi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "İBB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtAdi;
+        private System.Windows.Forms.TextBox TxtTanim;
+        private System.Windows.Forms.TextBox TxtSayi;
+        private System.Windows.Forms.DateTimePicker Tarih;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnListele;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.Button BtnAra;
+        private System.Windows.Forms.Button BtnTemizle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Ado.Net/Ado.Net(Prosedursuz)/Ado.Net(Prosedursuz)/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ado.Net_Prosedursuz_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("Server=DESKTOP-6QG82K9\\SQLEXPRESS;Database=IbbIstirak;integrated security=true;");
+        //SqlConnection connection = new SqlConnection("Server=DESKTOP-6QG82K9\SQLEXPRESS;Database=IbbIstirak;uid=kullaniciadi;pwd=sifre;");
+
+        public void List()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("Select * from Istirakler", connection); //Veri çekmek için kullanılır. Verirleri düzensiz çeker.
+            //Verileri düzenli hale getirmeliyiz
+            DataTable table = new DataTable(); // Düzensiz veriyi düzenli hale getirir.
+            adapter.Fill(table);
+            dataGridView1.DataSource = table; // datagridview içerisine düzenli veriyi attık.
+        }
+
+        public void MyClearMethod()
+        {
+            TxtAdi.Clear();
+            TxtTanim.Clear();
+            TxtSayi.Clear();
+            Tarih.Value = DateTime.Now;
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            List();
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into Istirakler (IstirakAdi, IstirakTanim, CalisanSayisi, Tarih) values (@IstirakAdi, @IstirakTanim, @CalisanSayisi, @Tarih)", connection);//SQL e veri göndermek veya veri güncellemek için kullanılır.
+            command.Parameters.AddWithValue("@IstirakAdi", TxtAdi.Text);
+            command.Parameters.AddWithValue("@IstirakTanim", TxtTanim.Text);
+            command.Parameters.AddWithValue("@CalisanSayisi", Convert.ToInt32(TxtSayi.Text));
+            command.Parameters.AddWithValue("@Tarih", Convert.ToDateTime(Tarih.Text));
+            command.ExecuteNonQuery(); // Insert, update, delete işlemlerinde kesinlikle kullanılır.
+            // ExecuteNonQuery() yukarıdaki yazma işlemini gerçekleştirir. Yazma işlemi başarılı olduysa 1, değilse 0 değeri döndüren bir methodtur.
+            connection.Close();
+
+            List();
+            MyClearMethod();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.CurrentRow;
+            TxtAdi.Tag = row.Cells["IstirakID"].Value.ToString();
+            TxtAdi.Text = row.Cells["IstirakAdi"].Value.ToString();
+            TxtTanim.Text = row.Cells["IstirakTanim"].Value.ToString();
+            TxtSayi.Text = row.Cells["CalisanSayisi"].Value.ToString();
+            Tarih.Text = row.Cells["Tarih"].Value.ToString();
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("update Istirakler set IstirakAdi=@IstirakAdi, IstirakTanim=@IstirakTanim, CalisanSayisi=@CalisanSayisi, Tarih=@Tarih where IstirakID=@IstirakID", connection); //insert, update, delete yaparken kullanılır.
+            command.Parameters.AddWithValue("@IstirakID", TxtAdi.Tag);
+            command.Parameters.AddWithValue("@IstirakAdi", TxtAdi.Text);
+            command.Parameters.AddWithValue("@IstirakTanim", TxtTanim.Text);
+            command.Parameters.AddWithValue("@CalisanSayisi", Convert.ToInt32(TxtSayi.Text));
+            command.Parameters.AddWithValue("@Tarih", Convert.ToDateTime(Tarih.Text));
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            List();
+            MyClearMethod();
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("delete from Istirakler where IstirakID=@IstirakID", connection); //insert, update, delete yaparken kullanılır.
+            command.Parameters.AddWithValue("@IstirakID", TxtAdi.Tag);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            List();
+            MyClearMethod();
+        }
+
+        private void BtnAra_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("Select * from Istirakler where IstirakAdi like '%" + TxtAdi.Text + "%'", connection); //Veri çekmek için kullanılır. Verirleri düzensiz çeker.
+            //Verileri düzenli hale getirmeliyiz
+            DataTable table = new DataTable(); // Düzensiz veriyi düzenli hale getirir.
+            adapter.Fill(table);
+            dataGridView1.DataSource = table; // datagridview içerisine düzenli veriyi attık.
+            MyClearMethod();
+        }
+
+        private void BtnTemizle_Click(object sender, EventArgs e)
+        {
+            MyClearMethod();
+        }
+    }
+}
